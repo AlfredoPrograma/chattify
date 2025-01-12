d3b2374464fada762107396f796a40bf481532b6
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-01-12 18:33:12 -0400

    client: set up io loop

--- client/main.go
+++ client/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"os"
 )
 
+const BUF_SIZE = 1024 // Around 256 characters
+
+type client struct{}
+
+func (c *client) Run() {
+	stdin := os.Stdin
+	stdout := os.Stdout
+
+	for {
+		buf := make([]byte, BUF_SIZE)
+		_, err := stdin.Read(buf)
+
+		if err != nil {
+			panic(err)
+		}
+
+		stdout.WriteString(string(buf))
+	}
+}
+
 func main() {
-	fmt.Println("Hello world")
+	c := client{}
+
+	c.Run()
 }
