b2a75493961c5cbac8b2dfbd185e890813d47933
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-01-12 18:52:25 -0400

    server: set up echo server

--- server/main.go
+++ server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+const BUF_SIZE = 1024 // Around 256 characters
+
+type server struct {
+	port string
+}
+
+func (s *server) handleConn(conn net.Conn) {
+	for {
+		buf := make([]byte, BUF_SIZE)
+		conn.Read(buf)
+		fmt.Println(string(buf))
+	}
+}
+
+func (s *server) Run() {
+	ln, err := net.Listen("tcp", s.port)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			panic(err)
+		}
+
+		s.handleConn(conn)
+	}
+}
 
 func main() {
-	fmt.Println("Hello Server")
+	s := server{port: ":9999"}
+
+	s.Run()
 }
