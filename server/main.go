40d01e2085e9927f4110e49946f8439deec098be
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-01-12 21:48:51 -0400

    client: improve connection registering and closing

--- server/main.go
+++ server/main.go
@@ -5,12 +5,49 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 )
 
 const BUF_SIZE = 1024 // Around 256 characters
 
+const (
+	DISCONNECT = iota
+	FORCED_DISCONNECT
+)
+
 type server struct {
-	port string
+	port        string
+	connections map[string]net.Conn
+	mu          sync.Mutex
+}
+
+func (s *server) registerConn(conn net.Conn) {
+	log.Printf("connection with peer %v started\n", conn.RemoteAddr())
+	s.connections[conn.RemoteAddr().String()] = conn
+}
+
+func (s *server) closeConn(addr string, disconnectKind int) {
+	s.mu.Lock()
+
+	conn, ok := s.connections[addr]
+
+	if !ok {
+		log.Printf("cannot close connection with unregistered peer %v\n", addr)
+		return
+	}
+
+	if disconnectKind == DISCONNECT {
+		err := conn.Close()
+
+		if err != nil {
+			log.Printf("cannot close connection with peer %v\n", addr)
+		}
+	}
+
+	delete(s.connections, addr)
+
+	log.Printf("connection with peer %v closed\n", addr)
+	defer s.mu.Unlock()
 }
 
 func (s *server) handleConn(conn net.Conn) {
@@ -20,7 +57,7 @@ func (s *server) handleConn(conn net.Conn) {
 
 		if err != nil || bytes == 0 {
 			if errors.Is(err, io.EOF) || bytes == 0 {
-				log.Printf("connection with peer %v closed\n", conn.RemoteAddr())
+				s.closeConn(conn.RemoteAddr().String(), FORCED_DISCONNECT)
 				break
 			}
 		}
@@ -43,6 +80,7 @@ func (s *server) Run() {
 
 	for {
 		conn, err := ln.Accept()
+		s.registerConn(conn)
 
 		if err != nil {
 			panic(err)
@@ -53,7 +91,7 @@ func (s *server) Run() {
 }
 
 func main() {
-	s := server{port: ":9999"}
+	s := server{port: ":9999", connections: map[string]net.Conn{}}
 
 	s.Run()
 }
