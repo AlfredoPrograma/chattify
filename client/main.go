3448d8f98817cfe6be490fb66f23289cecf716fc
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-01-12 21:28:28 -0400

    client: implement go routines to handle user input and server reading

--- client/main.go
+++ client/main.go
@@ -1,31 +1,59 @@
 package main
 
 import (
+	"io"
+	"net"
 	"os"
 )
 
 const BUF_SIZE = 1024 // Around 256 characters
 
-type client struct{}
+type client struct {
+	stdin  io.Reader
+	stdout io.Writer
+}
 
-func (c *client) Run() {
-	stdin := os.Stdin
-	stdout := os.Stdout
-
+func (c *client) readFromServer(conn net.Conn) {
 	for {
 		buf := make([]byte, BUF_SIZE)
-		_, err := stdin.Read(buf)
+		_, err := conn.Read(buf)
 
 		if err != nil {
 			panic(err)
 		}
 
-		stdout.WriteString(string(buf))
+		c.stdout.Write(buf)
 	}
 }
 
+func (c *client) writeToServer(conn net.Conn) {
+	for {
+		buf := make([]byte, BUF_SIZE)
+		_, err := c.stdin.Read(buf)
+
+		if err != nil {
+			panic(err)
+		}
+
+		conn.Write(buf)
+	}
+}
+
+func (c *client) Run() {
+	conn, err := net.Dial("tcp", ":9999")
+
+	if err != nil {
+		panic(err)
+	}
+
+	go c.writeToServer(conn)
+	go c.readFromServer(conn)
+
+	select {}
+}
+
 func main() {
-	c := client{}
+	c := client{os.Stdin, os.Stdout}
 
 	c.Run()
 }
