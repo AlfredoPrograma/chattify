d8648ba5d57b5ac80c6e9af3da50d1367c293b6e
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-01-13 13:48:52 -0400

    server: implement command handling

--- server/main.go
+++ server/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"sync"
 )
 
+const COMMAND_PREFIX = "!"
+const MESSAGES_CUTSET = "\n\t\r\x00"
 const BUF_SIZE = 1024 // Around 256 characters
 
 const (
@@ -69,19 +73,50 @@ func (s *server) broadcast(msg []byte, conn net.Conn) {
 	defer s.mu.Unlock()
 }
 
+func (s *server) handleCommand(command []byte, conn net.Conn) bool {
+	switch string(command) {
+	case "exit":
+		s.closeConn(conn.RemoteAddr().String(), DISCONNECT)
+		return true
+	default:
+		log.Printf("[SYSTEM]: peer %s tried to execute invalid command %s\n", conn.RemoteAddr().String(), command)
+		_, err := conn.Write([]byte(fmt.Sprintf("Invalid command <%s>", command)))
+
+		if err != nil {
+			log.Printf("[SYSTEM]: cannot write over peer %v\n", conn.RemoteAddr())
+		}
+
+		return false
+	}
+}
+
 func (s *server) handleConn(conn net.Conn) {
 	for {
 		buf := make([]byte, BUF_SIZE)
-		bytes, err := conn.Read(buf)
+		bufBytes, err := conn.Read(buf)
+		cleanedBuf := bytes.Trim(buf, MESSAGES_CUTSET)
 
-		if err != nil || bytes == 0 {
-			if errors.Is(err, io.EOF) || bytes == 0 {
+		if err != nil || bufBytes == 0 {
+			if errors.Is(err, io.EOF) || bufBytes == 0 {
 				s.closeConn(conn.RemoteAddr().String(), FORCED_DISCONNECT)
 				break
 			}
 		}
 
-		s.broadcast(buf, conn)
+		isCommand := bytes.Index(cleanedBuf, []byte(COMMAND_PREFIX)) == 0
+
+		if isCommand {
+			command := bytes.TrimPrefix(cleanedBuf, []byte(COMMAND_PREFIX))
+			shouldBreak := s.handleCommand(command, conn)
+
+			if shouldBreak {
+				break
+			}
+
+			continue
+		}
+
+		s.broadcast(cleanedBuf, conn)
 	}
 }
 
